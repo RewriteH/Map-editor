f450e49073e166e3dfe1d290872d62305040e693
Nishij0u <shioi1337@gmail.com>
2020-08-24 13:09:45 +0500

    0.0.1 v

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    minifyjs = require('gulp-minify'),
+    deploy = require('gulp-gh-pages');
+
+gulp.task('sass', function() {
+    return gulp.src('src/styles/*.sass')
+        .pipe(sass({outputStyle: ''}))
+        .pipe(gulp.dest('src'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('script', function () {
+    return gulp.src('src/scripts/*.js')
+        .pipe(browserSync.reload({stream: true}))
+});
+
+
+gulp.task('build', async function() {
+    let buildHtml = gulp.src('src/*.html')
+        .pipe(gulp.dest('build'));
+    let buildCss = await gulp.src('src/styles/App.sass')
+        .pipe(sass({outputStyle: ''}))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('build/styles'));
+    let buildJs = gulp.src('src/scripts/*.js')
+        .pipe(minifyjs())
+        .pipe(gulp.dest('build/scripts'))
+});
+
+gulp.task('deploy', function() {
+    return gulp.src("./dist/**/*")
+    .pipe(deploy())
+})
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src/"
+        }
+    });
+});
+
+gulp.task('watch', function(){
+    gulp.watch('src/styles/*.sass', gulp.parallel('sass'))
+    gulp.watch('src/*.html', gulp.parallel('html'))
+    gulp.watch('src/scripts/*.js', gulp.parallel('script'))
+})
+
+gulp.task('default', gulp.parallel('sass', 'script', 'browser-sync', 'watch'));
+
+
---
+++ src/scripts/main.js
@@ -0,0 +1,162 @@
+var canvas = document.querySelector('.game-area__ctx')
+var ctx = canvas.getContext('2d')
+var panelWalls = document.querySelector('.game-panel__walls')
+var draggableWall = null
+
+const getSelectedWall = () => walls.find(e => e.active === true)
+const getDraggableWall = () => walls.find(e => e.draggable === true)
+
+const currArea = {
+    x: 50,
+    y: 50,
+    width: 150,
+    height: 150,
+    x1: this.x + this.width,
+    x2: this.y + this.height
+}
+
+let walls = []
+
+const getXcoords = e => currArea.x + e.x
+const getYcoords = e => currArea.y + e.y
+
+canvas.onmousedown = e => {
+    walls.forEach(wall => {
+        // (Клик > координат начала wall && клик < координат конца wall
+        if (e.layerX > getXcoords(wall) && e.layerX < getXcoords(wall) + wall.w) {
+            wall.active = true
+            wall.draggable = true
+        } else { wall.active = false }
+    }
+    )
+    loadWalls()
+}
+
+canvas.onmousemove = e => {
+    const wall = getDraggableWall()
+    if (wall) {
+        let newX = e.layerX - currArea.x - (wall.w / 2)
+        let newY = e.layerY - currArea.y - (wall.h / 2)
+        if (newX < 0) {
+            newX = 0
+        }
+        if (newX >= currArea.x + currArea.width) {
+            newX = currArea.x + currArea.width
+        }
+        if (newY < 0) {
+            newY = 0
+        }
+        if (newY >= currArea.y + currArea.height) {
+            newY = currArea.y + currArea.height
+        }
+        wall.x = newX
+        wall.y = newY
+        loadWalls()
+    }
+}
+
+document.onmouseup = e => {
+    const wall = getDraggableWall()
+    if (wall) wall.draggable = null
+}
+
+const setSelectedWall = e => {
+    walls.forEach(wall => {
+        if (+e.id === wall.id) {
+            wall.active = true
+        } else { wall.active = false }
+    }
+    )
+    loadWalls()
+}
+
+const wallChange = e => {
+    const r = walls.find(wall => wall.active === true)
+    r[e.name] = +e.value
+    loadWalls()
+}
+
+const addWall = wall => {
+    if (walls.find(e => e.id === wall.id)) return
+    walls.push(wall)
+    panelWalls.insertAdjacentHTML('beforeend',
+        `<div 
+        class="game-panel__wall game-panel-wall"
+        id="wall-${wall.id}"r
+        onclick="setSelectedWall(this)"
+    >
+        <div class="game-panel-wall__coords">
+            <span classname="game-panel">x:</span>
+            <input name="x" oninput="wallChange(this)" value="${wall.x}">
+            <span>y:</span>
+            <input name="y" oninput="wallChange(this)" value="${wall.y}">
+        </div>
+        <div class="game-panel-wall__size">
+            <span>width:</span>
+            <input  oninput="wallChange(this)" name="w" value="${wall.w}">
+            <span>height:</span>
+            <input oninput="wallChange(this)" name="h" value="${wall.h}">
+        </div>
+    </div>`
+    )
+}
+
+const updatePanel = wall => {
+    const panelWall = document.querySelector(`#wall-${wall.id}`)
+    for (let prop in wall) {
+        if (panelWall.querySelector(`input[name=${prop}]`)) {
+            panelWall.querySelector(`input[name=${prop}]`).innerHTML = wall[prop]
+        }
+    }
+}
+
+const updateArea = wall => {
+    ctx.beginPath()
+    addWall(wall)
+    ctx.rect(getXcoords(wall), getYcoords(wall), wall.w, wall.h);
+    if (wall.active) {
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 5
+        ctx.stroke();
+    }
+    ctx.fill()
+    ctx.closePath()
+}
+
+const loadWalls = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    walls.map(e => updateArea(e))
+}
+
+updateArea({ x: 0, y: 0, w: 15, h: 50, id: 1 })
+
+updateArea({ x: 50, y: 50, w: 15, h: 50, id: 2 })
+
+
+const moveWallKey = e => {
+    let wall = getSelectedWall()
+    if (wall) {
+        switch (e.code) {
+            case 'KeyD':
+            case 'ArrowRight':
+                wall.x += 1
+                break
+            case 'KeyA':
+            case 'ArrowLeft':
+                wall.x -= 1
+                break
+            case 'KeyS':
+            case 'ArrowDown':
+                wall.y += 1
+                break
+            case 'KeyW':
+            case 'ArrowUp':
+                wall.y -= 1
+                break
+        }
+    }
+    updatePanel(wall)
+    loadWalls()
+}
+
+document.addEventListener('keydown', moveWallKey)
